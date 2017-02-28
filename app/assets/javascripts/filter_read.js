b1457c68a4618ac9f44b0437834c83f2b99ce776
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-27 19:04:12 -0700

    JS filtering

--- app/assets/javascripts/filter_read.js
+++ app/assets/javascripts/filter_read.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('#read-button').on('click', filterByRead);
   $('#unread-button').on('click', filterByUnRead);
-
+  $('#filter-input').on('keyup', filterBySearch);
 })
 
 function filterByRead() {
@@ -15,5 +15,20 @@ function filterByUnRead() {
   var readLinks = $('#links-list').children('.denote-read');
   for (i = 0; i < readLinks.length; i++) {
     document.getElementById(readLinks[i].id).style.display = "none";
+  }
+}
+
+function filterBySearch() {
+  var input = this.value.toUpperCase();
+  var allLinks = $('#links-list').children();
+  var link, linkDiv;
+  for (var i = 0; i < allLinks.length; i++) {
+    linkDiv = document.getElementById(allLinks[i].id)
+    link = $('#' + allLinks[i].id).children('.link-url, .link-title').text()
+    if (link.toUpperCase().indexOf(input) > -1) {
+      linkDiv.style.display = ""
+    } else {
+      linkDiv.style.display = "none"
+    }
   }
 }
