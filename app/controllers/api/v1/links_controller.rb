20992696e72897bc65e4af203b76432254db98cd
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-27 22:01:05 -0700

    Refactor api links update action

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -12,27 +12,34 @@ class Api::V1::LinksController < ApplicationController
   def update
     @link = Link.find params[:id]
     if params[:read] == "true"
-      @link.assign_attributes link_params
-      just_read = @link.read_changed? && @link.read
-      if @link.save
-        Read.create(link: @link) if just_read
-        service = HotReadsService.new(@link)
-        service.update
-        render json: @link
-      else
-        render json: @link.errors.full_messages, status: 500
-      end
+      mark_as_read
     else
-      @link.assign_attributes link_params
-      if @link.save
-        render json: @link
-      else
-        render json: @link.errors.full_messages, status: 500
-      end
+      mark_as_unread
     end
   end
 
   private
+
+  def mark_as_read
+    @link.assign_attributes link_params
+    just_read = @link.read_changed? && @link.read
+    if @link.save
+      service = HotReadsService.new(@link)
+      service.update
+      render json: @link
+    else
+      render json: @link.errors.full_messages, status: 500
+    end
+  end
+
+  def mark_as_unread
+    @link.assign_attributes link_params
+    if @link.save
+      render json: @link
+    else
+      render json: @link.errors.full_messages, status: 500
+    end
+  end
 
   def link_params
     params.permit(:title, :url, :read)
