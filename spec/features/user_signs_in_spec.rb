652c5e0a74bd23051ede6ed14c1bf95cb98f0773
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-27 19:38:26 -0700

    Sad path tests create and sign in user

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -13,5 +13,38 @@ describe "user signs in" do
       expect(current_path).to eq(root_path)
       expect(page).to have_content("You have signed in.")
     end
+
+    it "does not log in a user without an email" do
+      user = User.create(email: "email@email.com", password: "password", password_confirmation: "password")
+
+      visit login_path
+      fill_in "password", with: user.password
+      click_on "Submit"
+
+      expect(page).to have_content("Log In Unsuccessful. Please try again.")
+    end
+
+    it "does not log in a user without a password" do
+      user = User.create(email: "email@email.com", password: "password", password_confirmation: "password")
+
+      visit login_path
+
+      fill_in "email", with: user.email
+      click_on "Submit"
+
+      expect(page).to have_content("Log In Unsuccessful. Please try again.")
+    end
+
+    it "does not log in a user without an account" do
+      visit login_path
+
+      fill_in "email", with: "email@email.com"
+      fill_in "password", with: "password"
+      click_on "Submit"
+
+      expect(page).to have_content("Log In Unsuccessful. Please try again.")
+    end
+
+
   end
 end
